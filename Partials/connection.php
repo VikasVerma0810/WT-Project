63812ed3f513771b90988f6e1470350aeefac484
proprathamesh <prathameshsyadav1712@gmail.com>
2023-08-18 18:24:18 +0530

    just file changes

--- Partials/connection.php
+++ Partials/connection.php
@@ -1,9 +1,9 @@
 <?php
     // connecting to a database this are default
     $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "Resort";
+    $username = "u215729812_Resort";
+    $password = "17122003@Resort";
+    $database = "u215729812_Resort";
     // create a connection
     $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
