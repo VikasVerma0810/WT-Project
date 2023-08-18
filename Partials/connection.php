cd1d5fd1f9b48d6a12f1bff5354a839866cd510b
proprathamesh <prathameshsyadav1712@gmail.com>
2023-08-18 18:02:46 +0530

    made login backend

---
+++ Partials/connection.php
@@ -0,0 +1,9 @@
+<?php
+    // connecting to a database this are default
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "Resort";
+    // create a connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+?>
